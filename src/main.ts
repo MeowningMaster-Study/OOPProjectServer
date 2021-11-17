c67dccb72e3b68fb5d525ccb9f6b6500b5e80b29
Andriy Semenistiy <46621668+MeowningMaster@users.noreply.github.com>
2021-11-17 13:21:32 +0200

    load env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
-import { config } from "https://deno.land/x/dotenv@v3.1.0/mod.ts";
+import "https://deno.land/x/dotenv@v3.1.0/load.ts";
 import createLogger from "./telegram.ts";
 import serve from "./server.ts";
 
-config();
 const { PORT: port, BOT_TOKEN: botToken } = Deno.env.toObject();
 const log = createLogger(botToken);
 await serve(Number(port), log);
